a6437adc429b986c4dc31430b8c81180efe48e11
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-09-06 23:35:10 +0300

    feature/entity-base: add solution entity feature/entity-base: add base entity

--- domains/user/entity.go
+++ domains/user/entity.go
@@ -1,17 +1,10 @@
 package user
 
-import (
-	"time"
-
-	"github.com/google/uuid"
-)
+import "github.com/code-cage-dev/api/pkg/entity"
 
 type Entity struct {
-	ID        uuid.UUID `json:"id"`
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	GithubID  string    `json:"github_id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
+	entity.Base
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	GithubID string `json:"github_id"`
 }
